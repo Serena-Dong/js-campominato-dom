35af5f284d8e71372cfb44b7dcc12b4ea78abb8f
Serena-Dong <serenadong3159@icloud.com>
2022-12-19 12:54:33 +0100

    creazione array per contare i numeri cliccati

--- js/script.js
+++ js/script.js
@@ -45,6 +45,12 @@ form.addEventListener('submit', function(event){
         pagina.appendChild(grid);
     }
 
+
+
+
+
+
+    const clickedNumbers = [];
     
     for (let i = 0; i < totalCells; i++){
         
@@ -53,12 +59,33 @@ form.addEventListener('submit', function(event){
 
         //Aggiungo event listener per il click
         cell.addEventListener('click', function() {
-            cell.classList.toggle('clicked');
-            console.log(i + 1);
+            cell.classList.add('clicked');
+            clickedNumbers.push( i + 1);
+            console.log(clickedNumbers.length);
+
         });
 
-        //Appendo in pagina
-        grid.appendChild(cell);
+    //Appendo in pagina
+    grid.appendChild(cell);
 
     }
 })
+/* # MILESTONE 1
+Prepariamo "qualcosa" per tenere il punteggio dell'utente.
+Quando l'utente clicca su una cella, incrementiamo il punteggio.
+Se riusciamo, facciamo anche in modo da non poter più cliccare la stessa cella.
+
+# MILESTONE 2
+Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e il massimo di caselle disponibili.
+Generiamoli e stampiamo in console per essere certi che siano corretti
+
+# MILESTONE 3
+Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, controllando se il numero di cella è presente nell'array di bombe. Se si, la cella diventa rossa (raccogliamo il punteggio e e scriviamo in console che la partita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
+
+# MILESTONE 4
+Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare se il punteggio incrementato ha raggiunto il punteggio massimo perchè in quel caso la partita termina. Raccogliamo quindi il messaggio è scriviamo un messaggio appropriato.
+(Ma come stabiliamo quale sia il punteggio massimo?)
+
+# MILESTONE 5
+Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba o se perchè l'utente ha raggiunto il punteggio massimo. Dobbiamo poi stampare in pagina il punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
+*/
